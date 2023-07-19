b5462eb16c0530deadadf4c1d1f9a3b2ca22b4e8
Dixon A <dixona2023@icloud.com>
2023-07-20 00:26:45 +0530

    Range operators

--- Swift Theory.playground/Pages/Randomisation.xcplaygroundpage/Contents.swift
+++ Swift Theory.playground/Pages/Randomisation.xcplaygroundpage/Contents.swift
@@ -1,52 +1,52 @@
-//Randomisation
+///# Randomisation
 
 
-//Intiger
+///#Intiger
 
 let randomNumber = Int.random(in: 0...3) // alphabet[Int.random(in: 0...25)]
 print(randomNumber)
-/*  Int.random(in: lower bound ... upper bound) in this 'Closed range Operator' case0,1,2 or 3 is printed */
+///* Int.random(in: lower bound ... upper bound) in this 'Closed range Operator' case0,1,2 or 3 is printed
 
 let randomNumber1 = Int.random(in: 0..<3)
 print(randomNumber1)
-/* Int.random(in: lower bound ..< upper bound) in this case 'Half open range Operator' 3 is not printed  */
+///* Int.random(in: lower bound ..< upper bound) in this case 'Half open range Operator' 3 is not printed.
 
-//Float
+///#Float
 
 let randomNumber2 = Float.random(in: 0...2)
 print(randomNumber2)
 let randomNumber3 = Float.random(in: 0..<2)
 print(randomNumber3)
 
-//Double
+///#Double
 
 let randomNumber4 = Double.random(in: 0...1)
 print(randomNumber4)
 let randomNumber5 = Double.random(in: 0..<1)
 print(randomNumber5)
 
-//Bool
+///#Bool
 
 let randomNumber6 = Bool()
 print(randomNumber6)
-/* in the case Bool there is no range, it's either  true or false*/
+///* In the case Bool there is no range, it's either  true or false
 
 
-//Shuffle method
+///#Shuffle method
 
 var anArray = [0, 1, 2, 3, 4, 5 ,6, 7]
 anArray.shuffled()
-/*if anArray is a constant it can't be shuffled, in that case declare a var that can be used in the future */
+///* If anArray is a constant it can't be shuffled, in that case declare a var that can be used in the future
 print(anArray)
 //or
 let anArray1  = [0, 1, 2, 3, 4, 5 ,6, 7]
 var shuffledArray = anArray1.shuffled()[7]
-/* insteadb of shufflig the whole array just pick a random positon from the arrray */
+///* instead of shuffling the whole array just pick a random position from the array
 print(shuffledArray)
 //print("\(anArray1.shuffled())")
 
 
-//Random Element method
+///# Random Element method
 
 let anArray2 = [1, 2, 3, 4, 5, 6]
 let shuffledArray1 = anArray2.randomElement()!
@@ -55,7 +55,7 @@ print(shuffledArray1)
 
 //Example Password Generator
 
-/*Random Element method*/
+///# Random Element method
 let alphabet = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
 
 let password = alphabet.randomElement()! + alphabet.randomElement()! + alphabet.randomElement()! + alphabet.randomElement()! + alphabet.randomElement()! + alphabet.randomElement()!
@@ -69,7 +69,7 @@ let password1 = alphabet1[Int.random(in: 0...25)] + alphabet1[Int.random(in: 0..
 
 print(password1)
 
-/*Shuffled Method 8*/
+///# Shuffled Method
 let alphabet2 = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
 
 let password2 = alphabet2.shuffled()[25] + alphabet2.shuffled()[25] + alphabet2.shuffled()[25] + alphabet2.shuffled()[25] + alphabet2.shuffled()[25] + alphabet2.shuffled()[25]
--- Swift Theory.playground/Pages/Range Operators.xcplaygroundpage/Contents.swift
+++ Swift Theory.playground/Pages/Range Operators.xcplaygroundpage/Contents.swift
@@ -1,9 +1,46 @@
-//Range Operator
+///# Range Operator
 
-// Closed range Operator
-/* 1 ... 3 is a closedRange, which is inclusive of 1 and 3 */
-//Half open range Operator
-/* 1 ..< 3 is a halfOpenRange, which excludes 3 */
-//One Sided range operator
-/* ...3 or 1... is an oneSidedRange, which upto and including 3 but less than 3, and from 1 to any number*/
 
+///*In Swift, range operators are used to define a range of values. Ranges are useful for iterating over a sequence of values, extracting subranges from arrays or strings, and performing other operations on a continuous set of values. There are three range operators in Swift: the closed range operator, the half-open range operator, and the one-sided range operator.
+
+///# Closed Range Operator (...):
+
+///*The closed range operator (...) creates a range that includes both the start and end values. It forms a closed interval where the end value is also part of the range.
+
+let closedRange = 1...5
+print(closedRange) // Output: 1...5
+
+
+///#Half-Open Range Operator (..<):
+
+///*The half-open range operator (..<) creates a range that includes the start value but excludes the end value. It forms a half-open interval.
+
+let halfOpenRange = 1..<5
+print(halfOpenRange) // Output: 1..<5
+
+
+///#One-Sided Range Operator (a... or ...b):
+///
+///*The one-sided range operator creates a range that is either unbounded at the start or unbounded at the end. It is used when you want to define a range that extends from the start of a sequence to a specific value or from a specific value to the end of a sequence.
+
+let rangeFromOne = 1...
+print(rangeFromOne) // Output: 1...
+
+let rangeUpToFive = ...5
+print(rangeUpToFive) // Output: ...5
+
+///*Range operators are commonly used in various scenarios, such as looping through a sequence, extracting subranges from arrays or strings, slicing data, and more. Here are a few examples of using ranges:
+
+
+let numbers = 1...10
+
+for number in numbers {
+    print(number)
+}
+// Output: 1, 2, 3, 4, 5, 6, 7, 8, 9, 10
+
+let fruits = ["Apple", "Banana", "Orange", "Grapes", "Mango"]
+let slicedFruits = fruits[1..<4]
+print(slicedFruits) // Output: ["Banana", "Orange", "Grapes"]
+
+///*Range operators are an essential part of Swift, and they provide a concise and expressive way to work with ranges of values in a wide range of scenarios.
