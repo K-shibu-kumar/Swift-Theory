d59b91713f600663092554b9e72119503aaee038
Dixon A <dixona2023@icloud.com>
2023-07-20 20:31:53 +0530

    Typealias explained

--- Swift Theory.playground/Pages/TypeAlias.xcplaygroundpage/Contents.swift
+++ Swift Theory.playground/Pages/TypeAlias.xcplaygroundpage/Contents.swift
@@ -1,13 +1,28 @@
-// TypeAlias
+///# TypeAlias
+import Foundation
 
-/*
- 
- A type alias allows you to provide a new name for an existing data type into your program. After a type alias is declared, the aliased name can be used instead of the existing type throughout the program.
+///*In Swift, type-alias is a keyword used to give a new name (alias) to an existing data type. It allows you to create a custom name for a data type, making your code more readable and expressive. type-alias is particularly useful when you have complex data types or when you want to make your code more self-descriptive.
 
- Type alias do not create new types. They simply provide a new name to an existing type.
+//The basic syntax for type-alias in Swift is as follows:
 
- The main purpose of typeAlias is to make our code more readable, and clearer in context for human understanding.
 
- We can combine more than one protocols in some cases using type alias : Decodable, Encodable = Codable
-  
- */
+//typealias NewName = ExistingType
+//Here's an example of using typealias:
+
+typealias Coordinate = (x: Double, y: Double)
+
+func distance(from point1: Coordinate, to point2: Coordinate) -> Double {
+    let distanceX = point2.x - point1.x
+    let distanceY = point2.y - point1.y
+    return sqrt(distanceX * distanceX + distanceY * distanceY)
+}
+
+let pointA: Coordinate = (x: 2.0, y: 3.0)
+let pointB: Coordinate = (x: 5.0, y: 7.0)
+
+let distances = distance(from: pointA, to: pointB)
+print("Distance between pointA and pointB: \(distances)")
+
+///*In this example, we define a typealias Coordinate for the tuple type (x: Double, y: Double). By using typealias, we give a more meaningful and descriptive name to the tuple type, making the code more readable.
+
+///*Using typealias can make your code easier to understand, especially when you are working with complex types or long type names. It can also improve code maintainability by providing clear and concise type names throughout your codebase.
