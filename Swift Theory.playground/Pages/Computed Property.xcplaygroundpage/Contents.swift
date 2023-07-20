9d744f4cfebf14e8552812c7b3debb4c8bd11a3d
Dixon A <dixona2023@icloud.com>
2023-07-20 20:26:19 +0530

    Computed property in swift explained

--- Swift Theory.playground/Pages/Computed Property.xcplaygroundpage/Contents.swift
+++ Swift Theory.playground/Pages/Computed Property.xcplaygroundpage/Contents.swift
@@ -1,21 +1,45 @@
-// Computed Property
+///# Computed Property
 
-/*A Computed Property provides a getter and an optional setter to indirectly access other properties and values. It can be used in several ways.
+///*In Swift, a computed property is a property that does not store an underlying value directly but instead provides a getter and optionally a setter to compute its value on-the-fly based on other properties or logic. Computed properties are declared using the var keyword, but instead of providing a value directly, you provide a getter and/or a setter to calculate the value when it is accessed or set.
 
-A common use-case is to derive value from other properties. In the following example, we’re creating a filename based on the name and file extension:
- 
- Conditions:- A data type, return syntax, and computation should be provided.
- 
-*/
-struct Content {
-    var name: String
-    let fileExtension: String
+//Here's the basic syntax for declaring a computed property in Swift:
+
+//  var propertyName: PropertyType {
+//      get {
+//          // Code to compute the property value and return it
+//          return computedValue
+//      }
+//      set(newValue) {
+//          // Code to update the property based on the new value
+//          // (Optional: If you want to provide a setter)
+//      }
+//  }
+
+//Here's an example of a computed property to calculate the area of a circle:
+
+import Foundation
+
+class Circle {
+    var radius: Double
+
+    init(radius: Double) {
+        self.radius = radius
+    }
     
-    // A computed property to generate a filename.
-    var filename: String {
-        return name + "." + fileExtension
+    var area: Double {
+        get {
+            return Double.pi * radius * radius
+        }
     }
 }
 
-let content = Content(name: "swiftlee-banner", fileExtension: "png")
-print(content.filename) // Prints: "swiftlee-banner.png"
+let circle = Circle(radius: 5.0)
+print("Area of the circle: \(circle.area)")
+
+///*In this example, we define a Circle class with a computed property area. The area property does not store a value directly but calculates it based on the radius property of the Circle instance. The getter method calculates the area of the circle using the formula π * radius * radius.
+
+///*When we access the area property using circle.area, the getter is automatically called to compute the area based on the current value of the radius property.
+
+///*Note that computed properties must always provide a getter. If you don't need a setter, you can omit it entirely, and the property becomes read-only. If you provide a setter, the property becomes read-write, and you can update the property's value using the setter.
+
+///*Computed properties are a powerful feature in Swift that allows you to encapsulate complex calculations and logic within the property accessors, improving code readability and maintainability.
