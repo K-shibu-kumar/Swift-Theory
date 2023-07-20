5fa6df85aea33c9ee4bc72c2bf122cab8afc4881
Dixon A <dixona2023@icloud.com>
2023-07-20 18:47:48 +0530

    In Built functions updated

---
+++ Swift Theory.playground/Pages/Functions In-Built.xcplaygroundpage/Contents.swift
@@ -0,0 +1,64 @@
+///# Built-In Functions
+
+
+///*In Swift, the standard library provides a wide range of built-in functions that cover various operations, data types, and functionalities. Here are some commonly used built-in functions in Swift:
+
+///# Arithmetic Functions:
+
+abs(_:): Returns the absolute value of a numeric value.
+min(_:_:): Returns the minimum of two values.
+max(_:_:): Returns the maximum of two values.
+ceil(_:): Rounds up to the nearest whole number.
+floor(_:): Rounds down to the nearest whole number.
+round(_:): Rounds to the nearest whole number.
+sqrt(_:): Returns the square root of a value.
+pow(_:_:): Raises the first value to the power of the second value.
+
+
+///#Collection Functions:
+
+count(_:): Returns the number of elements in a collection.
+isEmpty(_:): Checks if a collection is empty.
+first(_:): Returns the first element of a collection.
+last(_:): Returns the last element of a collection.
+sorted(_:): Returns a sorted version of the collection.
+filter(_:_:): Returns a new collection containing elements that satisfy a given condition.
+map(_:_:): Transforms each element of a collection using a given closure and returns a new collection.
+                        
+                        
+///# String Functions:
+                        
+count(_:): Returns the number of characters in a string.
+isEmpty(_:): Checks if a string is empty.
+hasPrefix(_:_:): Checks if a string starts with a specific substring.
+hasSuffix(_:_:): Checks if a string ends with a specific substring.
+uppercased(_:): Converts a string to uppercase.
+lowercased(_:): Converts a string to lowercase.
+split(_:_:): Splits a string into an array of substrings based on a delimiter.
+                        
+                        
+///# Type Conversion Functions:
+                        ///
+Int(_:): Converts a value to an integer.
+Double(_:): Converts a value to a double.
+String(_:): Converts a value to a string.
+                        
+                        
+///# Range Functions:
+                        ///
+..<: Creates a half-open range that does not include the upper-bound value.
+...: Creates a closed range that includes both lower and upper bounds.
+                        
+                        
+///# Assertion Functions:
+                        
+assert(_:_:): Checks the given condition during debugging and terminates the program if the condition is false.
+                        
+                        
+///# Optional Handling Functions:
+                        
+Optional.some(_:): Wraps a value into an optional.
+Optional.none: Represents an optional with no value.
+
+
+///*These are just a few examples of the built-in functions available in Swift. The Swift standard library offers a rich set of functions that facilitate various programming tasks, making Swift a powerful and expressive language for developing iOS, macOS, watchOS, and tvOS applications.
