2ff50787047e4c072e0c08f6b42b4e14bc92f767
Dixon A <dixona2023@icloud.com>
2023-07-20 20:24:34 +0530

    Reformatted extension

--- Swift Theory.playground/Pages/Extentions.xcplaygroundpage/Contents.swift
+++ Swift Theory.playground/Pages/Extentions.xcplaygroundpage/Contents.swift
@@ -1,14 +1,11 @@
-// Extension
+///# Extension
 
 import Foundation
 import UIKit
 
-/*
- In Swift, extensions provide a way to add new functionality to existing classes, structures, enumerations, or protocols. They allow you to extend the behaviour of types without subclassing, which is especially useful when you don't have access to the original source code or when you want to keep your code modular and organised.
+///*In Swift, extensions provide a way to add new functionality to existing classes, structures, enumerations, or protocols. They allow you to extend the behaviour of types without subclassing, which is especially useful when you don't have access to the original source code or when you want to keep your code modular and organised.
 
- To define an extension in Swift, you use the extension keyword followed by the name of the type you want to extend. Here's an example that adds a computed property to the Int type:
- 
- */
+///*To define an extension in Swift, you use the extension keyword followed by the name of the type you want to extend. Here's an example that adds a computed property to the Int type:
 
 extension Int {
     var squared: Int {
@@ -16,17 +13,13 @@ extension Int {
     }
 }
 
-/*
- In this example, we're extending the Int type and adding a computed property called squared. Now, any instance of Int can access this property, which calculates the square of the integer value.
- */
+///*In this example, we're extending the Int type and adding a computed property called squared. Now, any instance of Int can access this property, which calculates the square of the integer value.
 let number = 5
 print(number.squared) // Output: 25
 
 
+///*Extensions can also be used to add instance methods, static methods, initialisers, subscript, and conform to protocols. Here are a few examples:
 
-/*
- Extensions can also be used to add instance methods, static methods, initialisers, subscript, and conform to protocols. Here are a few examples:
- */
 extension String {
     func sayHello() {
         print("Hello, \(self)!")
@@ -68,23 +61,19 @@ print(numbers[safe: 10] ?? 0) // Output: nil
 //}
 
 
-/*In the last example, CustomProtocol is a protocol, and we're extending it to add a custom method called customMethod.
+///*In the last example, CustomProtocol is a protocol, and we're extending it to add a custom method called customMethod.
 
-Extensions in Swift are a powerful tool for enhancing the functionality of existing types and promoting code reuse and modularity. They allow you to add new methods, properties, and conformances without modifying the original type definition.
-*/
-
+///*Extensions in Swift are a powerful tool for enhancing the functionality of existing types and promoting code reuse and modularity. They allow you to add new methods, properties, and conformances without modifying the original type definition.
 
 
 
 
 
-// Example:- On inbuilt Data types
 
+///# Example:- On inbuilt Data types
 
 //let myRoundedDouble = String(format: "%.1f", myDouble) //Mark:- Double rounded to String
-
 //print(myDouble.rounded()) //Mark:- here the double is rounded in to Int
-
 //In this particular scenario we can create an Extension to make this feature work
 // Rounding the Double in to x places
 
@@ -118,7 +107,7 @@ anotherDouble.round(to: 1)
 
 
 
-// Example:- First extension , then implementation
+///# Example:- First extension , then implementation
 
 
 extension UIButton {
@@ -139,11 +128,4 @@ let button1 = UIButton(frame: CGRect(x: 0, y: 0, width: 80, height: 80))
 button1.backgroundColor = .green
 button1.makeCircular() // We can now access the extension we created in to any UIButton
 
-
-
-
-
-// Example :- Protocol Extension
-
-//Refer protocols .Swift
 
