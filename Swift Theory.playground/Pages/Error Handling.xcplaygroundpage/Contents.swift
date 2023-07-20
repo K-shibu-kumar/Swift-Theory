e6bc499cbc46ea5f1ae8e8c02f9a4715ab6ad791
Dixon A <dixona2023@icloud.com>
2023-07-20 20:04:41 +0530

    Error handling Note added

--- Swift Theory.playground/Pages/Error Handling.xcplaygroundpage/Contents.swift
+++ Swift Theory.playground/Pages/Error Handling.xcplaygroundpage/Contents.swift
@@ -1,30 +1,50 @@
-// Error Handling
+///# Error Handling
 
-/*
- Error handling is the process of responding to and recovering from error conditions in your program. Swift provides first-class support for throwing, catching, propagating, and manipulating recoverable errors at runtime.
- 
- 
-// Here’s how you handle the error in Swift:
 
- let fileManager = FileManager.default
- let fromURL = URL(fileURLWithPath: "/path/to/old")
- let toURL = URL(fileURLWithPath: "/path/to/new")
- do {
-     try fileManager.moveItem(at: fromURL, to: toURL)
- } catch let error as NSError {
-     print("Error: \(error.domain)")
- }
- 
- 
- You can also use the do-catch statement to match on specific Cocoa error codes to differentiate possible failure conditions:
+///*Error handling in Swift allows you to handle and recover from errors that occur during the execution of your code. Errors in Swift are represented using the Error protocol or any custom type that conforms to Error. When an error occurs, you can use error handling to gracefully handle the error and take appropriate actions.
 
- do {
-     try fileManager.moveItem(at: fromURL, to: toURL)
- } catch CocoaError.fileNoSuchFile {
-     print("Error: no such file exists")
- } catch CocoaError.fileReadUnsupportedScheme {
-     print("Error: unsupported scheme (should be 'file://')")
- }
- 
- 
- */
+///*Error handling in Swift is done using the do-catch statement. The basic syntax is as follows:
+
+//  do {
+//      // Code that can potentially throw an error
+//      try expression
+//  } catch pattern1 {
+//      // Code to handle the error matching pattern1
+//  } catch pattern2 where condition {
+//      // Code to handle the error matching pattern2 and condition
+//  } catch {
+//      // Code to handle any other error not caught by previous catch blocks
+//  }
+
+//Here's an example of using error handling in Swift:
+
+enum MathError: Error {
+    case divisionByZero
+    case negativeNumber
+}
+
+func divide(_ a: Int, by b: Int) throws -> Int {
+    guard b != 0 else {
+        throw MathError.divisionByZero
+    }
+    
+    guard a >= 0 && b >= 0 else {
+        throw MathError.negativeNumber
+    }
+    
+    return a / b
+}
+
+do {
+    let result = try divide(10, by: 2)
+    print("Result: \(result)")
+} catch MathError.divisionByZero {
+    print("Cannot divide by zero.")
+} catch MathError.negativeNumber {
+    print("Both numbers should be positive.")
+} catch {
+    print("An error occurred: \(error)")
+}
+///*In this example, we define an enumeration MathError that conforms to the Error protocol. The divide(_:by:) function can throw errors of type MathError if the division by zero or if any of the numbers is negative. We use the do-catch statement to call the divide(_:by:) function and handle the errors appropriately.
+
+///*Error handling in Swift helps you write code that is more resilient and robust, making it easier to handle unexpected situations without crashing the program. It promotes better control flow and clearer error reporting, leading to more reliable and maintainable code.
