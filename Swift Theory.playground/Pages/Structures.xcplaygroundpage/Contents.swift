a796051356465eef0146e7d5a6290d610bd36f53
Dixon A <dixona2023@icloud.com>
2023-07-20 19:23:18 +0530

    Added elaborated notes about Structs

--- Swift Theory.playground/Pages/Structures.xcplaygroundpage/Contents.swift
+++ Swift Theory.playground/Pages/Structures.xcplaygroundpage/Contents.swift
@@ -1,8 +1,68 @@
-// Structures  ( Custom data type )
+import Foundation
+///# Structures  ( Custom data type )
 
 
-/* Defining a Struct. Also a simple struct has its default init
- in the below case name: String , citizens: [String] an Array, resources: [String : Int] a Dictionary*/
+///*In Swift, a structure is a user-defined data type that allows you to encapsulate related properties and behaviors together. Structures are value types, which means they are copied when passed around in code rather than being referenced like classes (which are reference types). Structures are used to create simple data models, and they play a significant role in Swift programming.
+
+//Here's the basic syntax for defining a structure in Swift:
+
+//struct MyStructure {
+//    // Properties (variables and constants)
+//    var property1: DataType
+//    var property2: DataType
+//
+//    // Initialiser (optional)
+//    init(property1: DataType, property2: DataType) {
+//        self.property1 = property1
+//        self.property2 = property2
+//    }
+//
+//    // Methods (functions)
+//    func myMethod() {
+//        // Code for the method
+//    }
+//}
+
+///*In the structure definition:
+
+///*struct: Keyword used to define a structure.
+///*MyStructure: The name of the structure. By convention, structure names should start with an uppercase letter.
+///*property1, property2: Properties of the structure. Properties define the data that the structure will hold.
+///*DataType: The data type of each property.
+///*init: An optional initialiser that allows you to set initial values for the properties.
+
+//Here's an example of a simple structure representing a 2D point:
+
+struct Point2D {
+    var x: Double
+    var y: Double
+    
+    init(x: Double, y: Double) {
+        self.x = x
+        self.y = y
+    }
+    
+    func distance(to otherPoint: Point2D) -> Double {
+        let distanceX = x - otherPoint.x
+        let distanceY = y - otherPoint.y
+        return sqrt(distanceX * distanceX + distanceY * distanceY)
+    }
+}
+
+///*You can create instances of a structure and access its properties and methods:
+
+let pointA = Point2D(x: 1.0, y: 2.0)
+let pointB = Point2D(x: 4.0, y: 6.0)
+
+print("Point A: x = \(pointA.x), y = \(pointA.y)")
+print("Point B: x = \(pointB.x), y = \(pointB.y)")
+
+let distance = pointA.distance(to: pointB)
+print("Distance between Point A and Point B: \(distance)")
+
+
+//Example
+
 struct Town {
     let name = "Erangel"
     var citizens = ["Noxid", "Ghost"] // this is a property of the struct
@@ -12,18 +72,18 @@ struct Town {
         print("Squad is ready to deploy!")// this is called a Method. func inside a struct
     }
 }
-/*Initialising a Struct*/
+//Initialising a Struct
 var myTown = Town()
-/* Accessing the struct */
+// Accessing the struct
 print(myTown.citizens)
 print("\(myTown.citizens[0]) has \(myTown.resources["Bandaids"]!) Bandaids. ")
-/* editing the Struct*/
+// editing the Struct
 myTown.citizens.append("Mad King")
 print(myTown.citizens) //or
 print(myTown.citizens.count)// counting the elements
 
 // Method (Adding a func inside a Struct)
-/* Func associated with a Struct or Class , is called a Method */
+// Func associated with a Struct or Class , is called a Method
 myTown.attack()
 
 
@@ -32,14 +92,15 @@ myTown.attack()
 
 
 
-// Struct with init
+///# Struct with init
 
-/* struct is like a blueprint we can use later to create multiple elements of an app. init let us tap in to the input properties of the struct
- think of struct is like a "BluePrint"
- and it has "Properties"
- and func as "Methods"
- access this struct to create multiple objects whenever we want */
-//defined a new struct
+///* Struct is like a blueprint we can use later to create multiple elements of an app. init let us tap in to the input properties of the struct
+///* Think of struct is like a "BluePrint"
+///* and it has "Properties"
+///* and func as "Methods"
+///* access this struct to create multiple objects whenever we want.
+
+///* Defined a new struct
 struct Town1 {
     let name: String
     var citizens: [String]
@@ -65,10 +126,10 @@ print(newTown.citizens)
 
 
 
-// Using self
+///# Using self
 
-/* .self refers to the property of struct . by default .self is a let keyword
- self is not referred in init initialiser */
+///* .self refers to the property of struct . by default .self is a let keyword
+///* .self is not referred in init initialiser
 struct Town2 {
     let name: String
     var citizens: [String]
@@ -106,27 +167,22 @@ print(NoOneSIland)
 
 NoOneSIland.attack()
 
-//Immutability
+///# Immutability
 
 
-/* when making changes to the properties of a struct within the struct using a method we have to mutate it by adding mutating before the method*/
+///* when making changes to the properties of a struct within the struct using a method we have to mutate it by adding mutating before the method.
 
 newTown1.collectPainkiller() // check the mutating func collectPainKiller ()
 print(newTown1.resources)
 
-/* like we said... self is by default created as a let keyword inside a struct. so in order to change the let keyword of self in to var to make changes inside a struct mutating is added.*/
+///* like we said... self is by default created as a let keyword inside a struct. so in order to change the let keyword of self in to var to make changes inside a struct mutating is added.
 
-// Methods inside struct
+///# Methods inside struct
 
-/* a struct has actually 2 methods .
- Plain Method : - that does not change anything about the struct
- Mutating Method : -  this method can change the state or properties of the struct */
+///* Plain Method : - that does not change anything about the struct
+///* Mutating Method : - .self func : this method can change the state or properties of the struct.
 
-
-
-
-
-//Exercise
+///* Exercise
 
 struct User {
     var  name: String
@@ -151,8 +207,6 @@ struct User {
     
 }
 
-
-
 //result
 
 var Richard = User(name: "Richard", email: nil, followers: 0, isActive: false)
@@ -170,15 +224,3 @@ print("\(musk.name) has \(musk.followers) followers")
 musk.isActive = false
 musk.logStatus()
 
-
-// Difference with Classes
-
-/*
- Immutable : - whenever we make changes in a struct , it destroys the old struct and creates an entire new struct
- 
- Passed by Value : - Meaning we can have more than one var pointing to the same object. And that var can be played around.
- */
-
-
-
-// you can set class or struct as a input type of another class or struct: struct MyStruct(parameter : Class) {}      like so. But only a class can inherit
