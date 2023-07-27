38893eb3e828418e25dc02013ed11d22dbe60276
Dixon A <dixona2023@icloud.com>
2023-07-27 19:45:30 +0530

    New Topic added

---
+++ Swift Theory.playground/Pages/Some Useful Shortcuts.xcplaygroundpage/Contents.swift
@@ -0,0 +1,14 @@
+///# Change View Modal transition in to full screen transition with back button
+// Select the VC(yellow button) -> Editor -> Embed in -> Navigation controller
+
+
+///# All the way to root VC
+// Syntax: navigationController?.popToRootViewController(animated: true)
+
+
+///# Hide back button in VC
+// Syntax: navigationItem.hidesBackButton = true
+
+
+///# Change or Show a title in VC
+// Syntax: title = "your title"
