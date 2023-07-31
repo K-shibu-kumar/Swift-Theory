a0fae48a148d24b6cf5555a70d80031c964010db
Dixon A <dixona2023@icloud.com>
2023-07-31 17:37:50 +0530

    Initial commit

---
+++ Swift Theory.playground/Pages/Table View.xcplaygroundpage/Contents.swift
@@ -0,0 +1,13 @@
+///# Table View Controller
+
+// :- Create a ViewController separately
+// :- Add a Table view customisable
+// :- Set the identifier (this works like a image view)
+// :-
+
+
+
+
+///# UITableViewDataSource
+
+///* Use this to customise the cell we created , supports upto iOS 13
