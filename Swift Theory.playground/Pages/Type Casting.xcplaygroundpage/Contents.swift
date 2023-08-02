419dadf2c69c6f721627ed14ae3f30e242589fcf
Dixon A <dixona2023@icloud.com>
2023-08-02 12:14:47 +0530

    Down, Force, Up casting notes , Any, AnyObject, NSObject notes added

--- Swift Theory.playground/Pages/Type Casting.xcplaygroundpage/Contents.swift
+++ Swift Theory.playground/Pages/Type Casting.xcplaygroundpage/Contents.swift
@@ -12,21 +12,21 @@
 //Here's how you can use the "as" operator:
 
 // Example classes
-class Animal {}
-class Dog: Animal {}
-class Cat: Animal {}
+class Animal1 {}
+class Dog: Animal1 {}
+class Cat: Animal1 {}
 
-let animal: Animal = Dog()
+let animal1: Animal1 = Dog()
 
 // Conditional downcasting (as?)
-if let dog = animal as? Dog {
+if let dog = animal1 as? Dog {
     print("It's a dog!")
 } else {
     print("It's not a dog.")
 }
 
 // Forced downcasting (as!)
-let dog = animal as! Dog
+let dog = animal1 as! Dog
 print("It's definitely a dog!")
 
 
@@ -34,7 +34,7 @@ print("It's definitely a dog!")
 
 ///*The "is" operator is used for checking the type of an instance without performing a type conversion. It returns a boolean value indicating whether the instance is of the specified type.
 
-let anotherAnimal: Animal = Cat()
+let anotherAnimal: Animal1 = Cat()
 
 if anotherAnimal is Dog {
     print("It's a dog!")
@@ -46,3 +46,195 @@ if anotherAnimal is Dog {
 
 
 ///*Type casting is useful when you work with collections of different types or when you want to interact with objects that conform to shared protocols without knowing their exact type. Always use "as?" when the type conversion might fail to avoid runtime crashes, unless you are absolutely sure the conversion will succeed, in which case you can use "as!".
+
+
+///# Another example with Explanation:-
+
+
+import Foundation
+
+class Animal {
+    var name: String
+    init (n: String) {
+        name = n
+    }
+}
+
+class Human: Animal {
+    func code() {
+        print("typing something....")
+    }
+}
+
+class Fish: Animal {
+    func breathUnderWater(){
+        print("Breathing under water....")
+    }
+}
+
+let me = Human(n: "Noxid")
+let avatar = Human(n: "Mad King")
+let nemo = Fish(n: "Nemo")
+
+let squad = [me, avatar, nemo]// Common attribute :Animal Superclass
+
+
+
+
+
+///# "is" operator: Type Checking
+// this is useful when there is an uncertainty of data type
+if squad[0] is Human {
+    print("First member is a Human")
+}
+if squad[1] is Human {
+    print("Second member is a Human")
+}
+if squad[2] is Human {
+    print("Third member is also a Human")
+} else {
+    print("Not Human")
+}
+
+
+
+
+
+///# "as!" Operator: Forced Down Casting :- Forcing the type to one of the SubClass
+func findNemo(from animals: [Animal]) {
+    for animal in animals {
+        print(animal.name)// All members of Animal Class
+        if animal is Fish {
+            print(animal.name) // Finding member of Fish Class
+            //animal.breathUnderWater()// The animal here is of Animal Class, And we can't access to the subClass Fish yet.
+            
+            
+            
+            
+            ///# Force Down Casting
+            let fish = animal as! Fish
+            fish.breathUnderWater()
+            
+            
+            
+            
+            ///# "as" Type Casting :- Up Casting
+            // Changing subclass type in to superclass
+
+            let animalToFish = fish as Animal// changing subclass Fish to Animal of var fish
+        
+        }
+    }
+}
+
+findNemo(from: squad)
+
+
+///# When to use as?
+//let fish = squad[1] as! Fish
+//fish.breathUnderWater()
+// The reason compiler won't give any unless we run it because we are forcing and saying we know for sure.
+// Error at run time
+// If we are not sure if it can adapt to the specific Class to which we are down casting to, Then Use "as?"
+
+
+
+
+
+
+///# "as?" Down Casting:
+
+if let fish = squad[0] as? Fish {
+    fish.breathUnderWater()
+} else {
+    print("Casting has failed")
+}
+
+if let fish =  squad[1] as? Fish {
+    fish.breathUnderWater()
+} else {
+    print("Casting has failed")
+}
+
+if let fish = squad[2] as? Fish {
+    fish.breathUnderWater()
+} else {
+    print("Casting has failed")
+}
+
+
+
+
+
+
+
+
+
+
+///# Any , AnyObject, NSObject
+
+
+///# Any:-
+let num = 32
+let squadAny: [Any] = [me, avatar, nemo, num]
+// Classes, Structures or Any other data type
+
+///# AnyObject:-
+let squadAnyObject: [AnyObject] = [me, avatar, nemo]
+// Only of object from Class. It won't accept object from Struct. try changing one of the Classes from above and we end up in an error.
+// INT, String ... are a Struct
+
+///# NSObject:-
+let NSNum: NSNumber = 17
+let NSString: NSString = "IsString"
+
+let squadNSObject: [NSObject] = [NSNum, NSString]
+// Only NS object types are allowed
+
+
+
+///# Any
+
+///*In Swift, type casting is the process of converting one type of value to another type. The Any, AnyObject, and NSObject are three types commonly used for type casting in Swift. Let's explore each of them:
+
+///*Any is a type that represents values of any type, including both value types (e.g., Int, String, Bool) and reference types (e.g., classes). It can store any type of value, making it useful when you need to work with mixed types or in cases where the exact type is unknown at compile time.
+
+let anyValue: Any = 42
+if let intValue = anyValue as? Int {
+    print("The value is an Integer: \(intValue)")
+} else {
+    print("The value is not an Integer.")
+}
+
+
+
+///#AnyObject:
+///*AnyObject is a type that represents instances of any class type (i.e., reference types). You can use it when you need to work with objects that could be of different classes but share a common base class.
+
+class MyClass {
+    func myFunction() {
+        print("This is my function.")
+    }
+}
+
+let anyObjectValue: AnyObject = MyClass()
+if let myClassInstance = anyObjectValue as? MyClass {
+    myClassInstance.myFunction()
+} else {
+    print("The value is not an instance of MyClass.")
+}
+
+
+
+///#NSObject:
+///*NSObject is a class in Swift that is the base class for most Objective-C classes. It is often used when working with Objective-C APIs that require interoperability with Swift.
+
+let nsObjectValue: NSObject = NSString(string: "Hello, World!")
+if let stringValue = nsObjectValue as? String {
+    print("The value is a String: \(stringValue)")
+} else {
+    print("The value is not a String.")
+}
+
+
+///*It's essential to use type casting carefully and handle potential type mismatches or unknown types properly. Always prefer strong typing whenever possible, as it provides better compile-time checks and can make your code more robust. Type casting should be used judiciously and when necessary for working with mixed or dynamic types.
