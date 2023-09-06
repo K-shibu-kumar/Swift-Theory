cac18e83734d966ff3ad72ddc46b0914ea30dd02
Dixon A <dixona31@gmail.com>
2023-09-06 12:01:45 +0530

    Change in dir

--- Swift Theory.playground/Pages/Table View.xcplaygroundpage/Contents.swift
+++ Swift Theory.playground/Pages/Table View.xcplaygroundpage/Contents.swift
@@ -11,3 +11,23 @@
 ///# UITableViewDataSource
 
 ///* Use this to customise the cell we created , supports upto iOS 13
+
+
+
+///  or
+
+///# Adding A Table View as the default VC
+
+///# Add a TableView from library
+///# Set this as The 1st VC in storyboard
+///# In VC file-> Set the data type as UITableViewController
+///# here Xcode will take care of the delegates and data source automatically
+
+
+///# Cell
+
+///* To populate data in cell row:-
+// cell.textLabel.text = anArray[indexPath.row]
+
+
+
